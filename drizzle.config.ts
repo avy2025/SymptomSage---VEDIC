97cf9d8ed1ebcdb54a5a6dc889af3ec5d48ef089
avy2025 <168371819+avy2025@users.noreply.github.com>
2026-01-18 22:43:33 +0530

    imporved performance of database queries by optimizing indexing strategy and refining query logic.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,11 @@
+/// <reference types="node" />
+
 import { defineConfig } from "drizzle-kit";
 
 if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is missing. Please set it in your environment variables.");
+  throw new Error(
+    "DATABASE_URL is missing. Please set it in your environment variables."
+  );
 }
 
 export default defineConfig({
